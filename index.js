fa0489dd3c86ad6a8af86d0930a002bce1c391ae
Matt Redmon <matt.redmon@live.com>
2018-01-05 02:37:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ function increaseRankBy(n) {
 
 function deepestChild() {
   var grandNode = document.querySelector('#grand-node');
-  var deepChild = grandNode.children[0];
-  
+  var deep = grandNode.querySelectorAll('div')
+  deep = deep[deep.length - 1]
+  return deep
 }
